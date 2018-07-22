8f919b05c3b2a92c892876e0ff568a4977a76946
Nahuelgrc <nahuelgrc@gmail.com>
2018-07-22 17:16:55 -0300

    First API
    First API in order to get the cities

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id=1, name= "Buenos Aires"},
+                new { id=2, name= "Entre Ríos"},
+            });
+        }        
+    }
+}
